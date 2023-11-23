088c930c724081f03208209fcd40d2953ea9ccc2
Owen <osmour043@gmail.com>
2023-11-22 18:33:50 -0600

    birdfeeder functionality

--- src/Birdfeeder.py
+++ src/Birdfeeder.py
@@ -3,22 +3,29 @@ from BirdfeederDie import BirdfeederDie
 class Birdfeeder:
 
     def __init__(self):
-        self.food = list() # initialize
-        self.reroll() # roll for 1st time
+        self.in_feeder = list()
+        self.out_of_feeder = list()
+        self.can_be_rerolled = True
+        self.roll() # roll for 1st time
 
-    def reroll(self):
-        if len(set(self.food)) > 1:
-            raise Exception('Cannot reroll feeder if there is more than 1 type of food in it.')
+    def roll(self):
+        if not self.can_be_rerolled:
+            raise Exception(f'Bird feeder cannot be re-rolled. Currently has {self.in_feeder}.')
         
-        del self.food # clear the birdfeeder
-        self.food = [
+        self.in_feeder, self.out_of_feeder = list(), list()
+        self.in_feeder = [
             BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, 
             BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, 
             BirdfeederDie().faceup_side
         ]
+        self.can_be_rerolled = False
 
     def take(self, food):
-        if food not in self.food:
+        if food not in self.in_feeder:
             raise ValueError(f'{food} is not in the birdfeeder.')
+
+        if len(set(food)) <= 1:
+            self.can_be_rerolled = True
         
-        self.food.remove(food)
+        self.in_feeder.remove(food)
+        self.out_of_feeder.append(food)
--- src/ui/app.py
+++ src/ui/app.py
@@ -1,14 +1,53 @@
-from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QGridLayout, QLabel, QWidget, QHBoxLayout, QPushButton
-)
-from PyQt6.QtGui import QPixmap
 import sys
+sys.path.append('../')
+from Birdfeeder import Birdfeeder
+from Player import Player
+
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QGridLayout, QLabel, QWidget, QHBoxLayout, QPushButton,
+    QAbstractButton,
+)
+from PyQt6.QtGui import QPixmap, QPainter
+
+class PicButton(QAbstractButton):
+    def __init__(self, impath, parent=None):
+        super(PicButton, self).__init__(parent)
+        self.pixmap = QPixmap(impath)
+        self.impath = impath
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(event.rect(), self.pixmap)
+
+    def sizeHint(self):
+        return self.pixmap.size()
+    
+def clearLayout(layout):
+    while layout.count():
+        child = layout.takeAt(0)
+        if child.widget():
+            child.widget().deleteLater()
+        elif child.layout():
+            clearLayout(child)
+
+food = {
+    'Fish': './images/food_fish.png', 
+    'Rodent': './images/food_rodent.png', 
+    'Fruit': './images/food_fruit.png', 
+    'Invertebrate': './images/food_invertebrate.png', 
+    'Seed': './images/food_seed.png', 
+    'Invertebrate+Seed': './images/food_invertebrate+seed.png',
+}
 
 # Main window class ============================================================
 class WingspanAppWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('ca-caw! bird brain ca-caw!')
+
+        # Initialize all of the game element objects
+        self.birdfeeder = Birdfeeder()
+        self.player = Player([], [])
 
         # Initialize layouts
         self.centralLayout = QGridLayout()
@@ -59,10 +98,7 @@ class WingspanAppWindow(QMainWindow):
         infoLayout.addWidget(QLabel('Rd. 4', styleSheet='border: 1px solid black;'), 2, 3)
 
         # Birdfeeder Layout
-        self.birdfeederLayout.addWidget(QLabel('In feeder:', styleSheet='font-weight: bold;'), 0, 0)
-        self.birdfeederLayout.addWidget(QLabel(styleSheet='border: 1px solid black;'), 1, 0)
-        self.birdfeederLayout.addWidget(QLabel('Out of feeder:', styleSheet='font-weight: bold;'), 0, 1)
-        self.birdfeederLayout.addWidget(QLabel(styleSheet='border: 1px solid black;'), 1, 1)
+        self.drawBirdfeeder()
 
         # Bird card deck layout
         birdCardDeckLayout = QGridLayout(self.birdCardDeck)
@@ -72,17 +108,7 @@ class WingspanAppWindow(QMainWindow):
         birdCardDeckLayout.addWidget(QLabel(styleSheet='border: 1px solid black;'), 1, 2)
 
         # Food in hand layout
-        self.handFoodLayout.addWidget(QLabel('Food in hand', styleSheet='font-weight: bold;'), 0, 0)
-        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_invertebrate.png')), 0, 1)
-        self.handFoodLayout.addWidget(QLabel('x 0'), 0, 2)
-        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_seed.png')), 0, 3)
-        self.handFoodLayout.addWidget(QLabel('x 0'), 0, 4)
-        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_fish.png')), 1, 0)
-        self.handFoodLayout.addWidget(QLabel('x 0'), 1, 1)
-        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_rodent.png')), 1, 2)
-        self.handFoodLayout.addWidget(QLabel('x 0'), 1, 3)
-        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_fruit.png')), 1, 4)
-        self.handFoodLayout.addWidget(QLabel('x 0'), 1, 5)
+        self.drawFoodHand()
 
         # Bird cards in hand layout
         handBirdCardsLayout = QHBoxLayout(self.handBirdCards)
@@ -115,6 +141,64 @@ class WingspanAppWindow(QMainWindow):
         w = QWidget()
         w.setLayout(self.centralLayout)
         self.setCentralWidget(w)
+
+    #===============================================================================
+    def drawBirdfeeder(self):
+        clearLayout(self.birdfeederLayout)
+        self.inFeederLayout = QHBoxLayout()
+        self.outOfFeederLayout = QHBoxLayout()
+        self.takeFoodButton = QPushButton('Take')
+        self.takeFoodButton.clicked.connect(self.takeFood)
+        self.rollButton = QPushButton('Roll')
+        self.rollButton.clicked.connect(self.rollFeeder)
+        self.birdfeederLayout.addWidget(QLabel('In feeder:', styleSheet='font-weight: bold;'), 0, 0)
+        self.birdfeederLayout.addWidget(QLabel('Out of feeder:', styleSheet='font-weight: bold;'), 0, 1)
+        self.birdfeederLayout.addLayout(self.inFeederLayout, 1, 0)
+        self.birdfeederLayout.addLayout(self.outOfFeederLayout, 1, 1)
+        self.birdfeederLayout.addWidget(self.takeFoodButton, 2, 0)
+        self.birdfeederLayout.addWidget(self.rollButton, 2, 1)
+        for die in self.birdfeeder.in_feeder:
+            self.inFeederLayout.addWidget(QLabel(pixmap=QPixmap(food[die])))
+        for die in self.birdfeeder.out_of_feeder:
+            self.outOfFeederLayout.addWidget(QLabel(pixmap=QPixmap(food[die])))
+
+    #===============================================================================
+    def rollFeeder(self):
+        self.birdfeeder.roll()
+        self.drawBirdfeeder()
+
+    #===============================================================================
+    def takeFood(self):
+        from PyQt6.QtWidgets import QInputDialog
+        food, ok = QInputDialog().getItem(self, 'Choose food from birdfeeder', 'Choices', self.birdfeeder.in_feeder, 0, False)
+        if ok and food=='Invertebrate+Seed':
+            # Another popup to choose which one to take
+            food_to_remove = 'Invertebrate+Seed'
+            food, ok = QInputDialog().getItem(self, 'Choose food from birdfeeder', 'Choices', ['Invertebrate', 'Seed'])
+        elif ok:
+            food_to_remove = food
+        if ok:
+            # Take food from the feeder and put it into the player's hand
+            self.birdfeeder.take(food_to_remove)
+            self.player.food[food] += 1
+            # Redraw the board
+            self.drawBirdfeeder()
+            self.drawFoodHand()
+
+    #==========================================================================
+    def drawFoodHand(self):
+        clearLayout(self.handFoodLayout)
+        self.handFoodLayout.addWidget(QLabel('Food in hand', styleSheet='font-weight: bold;'), 0, 0)
+        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_invertebrate.png')), 0, 1)
+        self.handFoodLayout.addWidget(QLabel(f'x {self.player.food["Invertebrate"]}'), 0, 2)
+        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_seed.png')), 0, 3)
+        self.handFoodLayout.addWidget(QLabel(f'x {self.player.food["Seed"]}'), 0, 4)
+        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_fish.png')), 1, 0)
+        self.handFoodLayout.addWidget(QLabel(f'x {self.player.food["Fish"]}'), 1, 1)
+        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_rodent.png')), 1, 2)
+        self.handFoodLayout.addWidget(QLabel(f'x {self.player.food["Rodent"]}'), 1, 3)
+        self.handFoodLayout.addWidget(QLabel(pixmap=QPixmap('./images/food_fruit.png')), 1, 4)
+        self.handFoodLayout.addWidget(QLabel(f'x {self.player.food["Fruit"]}'), 1, 5)
 
 # Main loop ====================================================================
 app = QApplication(sys.argv)
