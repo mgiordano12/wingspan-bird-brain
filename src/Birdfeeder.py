901f20b2545b726e2fb4b3a28f32e630dbc4aea5
Owen <osmour043@gmail.com>
2023-11-27 19:43:16 -0600

    undo some of my Birdfeeder changes

--- src/Birdfeeder.py
+++ src/Birdfeeder.py
@@ -4,30 +4,23 @@ class Birdfeeder:
 
     def __init__(self):
         # TODO: We need to randomly seed the birdfeeder, which isn't trivial
-        self.food = list()
-        self.can_be_rerolled = True
+        self.food = list() # initialize
         self.reroll() # roll for 1st time
 
     def reroll(self):
-        # if not self.can_be_rerolled:
-        #     raise Exception(f'Bird feeder cannot be re-rolled. Currently has {self.food}.')
+        if len(set(self.food)) > 1:
+            raise Exception(f'Cannot reroll feeder if there is more than 1 type of food in it. Currently has {self.food}.')
         
         del self.food # clear the birdfeeder
         self.food = [
-            BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, 
-            BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, 
-            BirdfeederDie().faceup_side
+            BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, BirdfeederDie().faceup_side, 
+            BirdfeederDie().faceup_side, BirdfeederDie().faceup_side
         ]
-        self.can_be_rerolled = False
 
     def take(self, food):
         if food not in self.food:
-            raise ValueError(f'{food} is not in the birdfeeder. Birdfeeder has {self.food}')
-        
+            raise ValueError(f'{food} is not in the birdfeeder.')
         self.food.remove(food)
-
-        if len(set(self.food)) <= 1:
-            self.can_be_rerolled = True
 
     def __repr__(self):
         return f'Birdfeeder: {self.food}'
